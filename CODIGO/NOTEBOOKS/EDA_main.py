aa7b1f5841436d3c518687f8715a3f36e6381b29
ramasa987 <raulmarc.eus@gmail.com>
2026-05-15 12:37:52 +0200

    images

--- CODIGO/NOTEBOOKS/EDA_main.py
+++ CODIGO/NOTEBOOKS/EDA_main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.preprocessing import MinMaxScaler
+
 plt.style.use("seaborn-v0_8")
 sns.set_palette("tab10")
 from datetime import datetime
